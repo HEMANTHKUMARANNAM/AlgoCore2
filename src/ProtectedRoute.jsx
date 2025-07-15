ad2bb919e9d57adcac51503dd92c7a021cefc93b
hemanthkumarannam <hemanthkumar312004@gmail.com>
2025-07-15 10:37:01 +0530

    add

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -2,9 +2,15 @@ import React, { useEffect, useState } from "react";
 import { auth, database } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { ref, get } from "firebase/database";
+import { useAuth } from "./context/AuthContext";
 
-const ProtectedRoute = ({ children, requireAdmin = false , requireUser = false }) => {
+
+
+const ProtectedRoute = ({ children, requireAdmin = false, requireUser = false }) => {
   const [authStatus, setAuthStatus] = useState("loading"); // 'loading', 'unauthenticated', 'authenticated', 'unauthorized'
+
+  const { user } = useAuth();
+
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
@@ -13,26 +19,29 @@ const ProtectedRoute = ({ children, requireAdmin = false , requireUser = false }
         return;
       }
 
+
       // Get user's role from Realtime Database
       try {
         const userRef = ref(database, `Admins/${user.uid}`);
         const snapshot = await get(userRef);
 
-        if (snapshot.exists()) {
-          const isAdmin = true;
 
-          if (requireAdmin && !isAdmin) {
-            setAuthStatus("unauthorized");
-          }
-          else if (requireUser && isAdmin) {
-            setAuthStatus("unauthorized");
-          }
-          else {
-            setAuthStatus("authenticated");
-          }
-        } else {
-          setAuthStatus("unauthorized");
+        let isAdmin = false;
+
+        if (snapshot.exists()) {
+          isAdmin = true;
         }
+
+        if (requireAdmin && !isAdmin) {
+          setAuthStatus("unauthorized");
+          console.log("meow1");
+        } else if (requireUser && isAdmin) {
+          setAuthStatus("unauthorized");
+          console.log("meow2");
+        } else {
+          setAuthStatus("authenticated");
+        }
+
       } catch (error) {
         console.error("Error fetching user data:", error);
         setAuthStatus("unauthorized");
@@ -40,7 +49,7 @@ const ProtectedRoute = ({ children, requireAdmin = false , requireUser = false }
     });
 
     return () => unsubscribe();
-  }, [requireAdmin, requireUser]);
+  }, [requireAdmin, requireUser, user]);
 
   if (authStatus === "loading") return <div>Loading...</div>;
   if (authStatus === "unauthenticated") return <div>Sign-in required</div>;
